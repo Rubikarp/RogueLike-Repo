8bce564d71c143412c8280a337bcf46312f0d077
WilliamSchmitt120 <william.schmitt120@gmail.com>
2019-11-12 11:02:10 +0100

    Willi

--- RamboVersusRobot/Assets/_Script/WSH_Script/EnemyBase.cs
+++ RamboVersusRobot/Assets/_Script/WSH_Script/EnemyBase.cs
@@ -5,33 +5,117 @@ using UnityEngine;
 public class EnemyBase : MonoBehaviour
 {
 
-    public int health;
-    
-    
+    //Variables pour la Vitesse
+    [SerializeField] public int speed;
+
+    //Récupérer le rigidbody du robot et le transform du joueur
+    [SerializeField] public Rigidbody2D srb;
+    [SerializeField] public Transform player;
+
+    //Détécter quand le robot est endormi et sa zone de détéction
+    [SerializeField] private bool IsSleeping = true;
+    [SerializeField] public int detectionRange;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
     }
 
-    void Update()
+    private void Update()
     {
 
-        if (health <= 0)
+        //Détermine si le robot est réveillé ou non.
+        if (Vector2.Distance(transform.position, player.position) > detectionRange)
         {
 
-            GameObject.Destroy(gameObject);
+            IsSleeping = true;
 
+        } else if (Vector2.Distance(transform.position, player.position) < detectionRange)
+        {
+
+            IsSleeping = false;
+
+        }
+                
+    }
+
+    private void FixedUpdate()
+    {
+        
+        //Détermine ce que fait le robot quand il est réveillé ou endormi.
+        if (IsSleeping == false)
+        {
+            //Comportement réveillé
+            AwakenBehaviour();
+            
+
+        } else if (IsSleeping == true)
+        {
+            //Comportement Endormi
+            SleepingBehaviour();
+            
         }
 
 
     }
 
-    public void TakeDamage(int damage)
+
+    //Fonction pour détruire le robot.
+    protected void Destruction()
     {
 
-        health -= damage;
-        Debug.Log("damage TAKEN !");
+        GameObject.Destroy(gameObject);
+
+    }
+
+    //Fonction de Base pour faire bouger le robot d'une position vers une autre en ligne droite
+    protected void MoveToPosition(Transform destination, int speed)
+    {
+
+        srb.velocity = new Vector2((destination.position.x - transform.position.x) / speed, (destination.position.y - transform.position.y) / speed);
+
+    }
+
+    //Version de la fonction pour uniquement bouger la position horizontalement
+    protected void HoriMoveToPosition(Transform destination, int speed)
+    {
+
+        srb.velocity = new Vector2((destination.position.x - transform.position.x) / speed, 0);
+
+    }
+
+    //Version de la fonction pour uniquement bouger la position verticalement.
+    protected void VertiMoveToPosition(Transform destination, int speed)
+    {
+
+        srb.velocity = new Vector2(0, (destination.position.y - transform.position.y) / speed);
+
+    }
+
+    protected virtual void SleepingBehaviour()
+    {
+
+        //Debug.Log("Sleeping");
+        
+
+    }
+
+    protected virtual void AwakenBehaviour()
+    {
+
+        //Debug.Log("Awaken");
+ 
+
+    }
+
+    protected virtual void Attack()
+    {
+
+        //Debug.Log("Attack");
+
 
     }
 }
---
+++ RamboVersusRobot/Assets/_Script/WSH_Script/EnemyGround1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyGround1 : EnemyBase
+{
+
+    
+    protected override void AwakenBehaviour()
+    {
+
+        HoriMoveToPosition(player, speed);
+
+    }
+
+
+
+}
