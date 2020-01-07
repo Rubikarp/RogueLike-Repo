20c04a701342cc1d27c3e99b03d43f9519fbe8f8
Lolzxad <adelou001@hotmail.com>
2020-01-07 22:22:46 +0100

    Better platform + Lever DONE

--- RamboVersusRobot/Assets/_Script/AAY_Scripts/Level Design/Lever.cs
+++ RamboVersusRobot/Assets/_Script/AAY_Scripts/Level Design/Lever.cs
@@ -2,17 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Lever : MonoBehaviour
+namespace LevelDesign
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Lever : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        private float speed;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        private bool doorClosed = true;
+        private bool doorWillClose = false;
+        public GameObject door;
+        public float timer;
+
+        private void OnTriggerEnter2D (Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player" && doorClosed)
+            {
+                StartCoroutine(OpenDoor());
+            }
+        }
+        private void OnTriggerExit2D (Collider2D collision)
+        {
+            if (collision.gameObject.tag == "Player" && !doorWillClose)
+            {
+                StartCoroutine(CloseDoor());
+            }
+        }
+        IEnumerator OpenDoor()
+        {
+            doorClosed = false;
+            door.transform.Translate(Vector2.up * speed);
+            yield return new WaitForSeconds(1);          
+            StopCoroutine(OpenDoor());
+
+        }
+        IEnumerator CloseDoor()
+        {
+            doorWillClose = true;
+            yield return new WaitForSeconds(timer);          
+            door.transform.Translate(Vector2.down * speed);
+            yield return new WaitForSeconds(1);
+            doorClosed = true;
+            doorWillClose = false;
+            StopCoroutine(CloseDoor());
+        }
     }
 }
