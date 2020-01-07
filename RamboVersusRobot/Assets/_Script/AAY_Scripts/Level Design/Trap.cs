fca40527f14b82023c4eb22f4e36e60f4dd22cb8
Lolzxad <adelou001@hotmail.com>
2020-01-07 23:27:57 +0100

    WIP damage box

--- RamboVersusRobot/Assets/_Script/AAY_Scripts/Level Design/Trap.cs
+++ RamboVersusRobot/Assets/_Script/AAY_Scripts/Level Design/Trap.cs
@@ -2,17 +2,30 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trap : MonoBehaviour
+namespace LevelDesign
 {
-    // Start is called before the first frame update
-    void Start()
+    public class Trap : MonoBehaviour
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField]
+        private float knockback = 1.0f;
+
+        public Rigidbody2D playerRigidbody;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            playerRigidbody = GameObject.Find("Character").GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                Debug.Log("D");
+                playerRigidbody.AddForce(transform.up * knockback);
+            }           
+        }
     }
 }
